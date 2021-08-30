cc5fa4644ab6507b471be10ccf4ba85bab5052d2
NhaVoVan <38065969+vvnha@users.noreply.github.com>
2021-08-30 16:45:36 +0700

    first commit create RTC for FE

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import './App.scss';
 import Stream from './components/Stream';
-// import Live from './components/Live';
-// import Streamer from './components/Temp/streamer';
-// import Viewer from './components/Temp/viewer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 function App() {
   return (
@@ -18,12 +9,5 @@ function App() {
     </div>
   );
 }
-// function StreamLive() {
-//   return <Streamer />;
-// }
-
-// function ViewerLive() {
-//   return <Viewer />;
-// }
 
 export default App;
